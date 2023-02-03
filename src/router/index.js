802b49337c914b97bcfb95993569c6b7a8d1c26b
Magic0323 <magic_yw2001@163.com>
2023-02-03 21:44:34 +0800

    初始化配置

--- src/router/index.js
+++ src/router/index.js
@@ -13,15 +13,18 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: '登录页面',
+    component: () => import('../views/login/LoginView.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">这是login页面
+     <el-button class="btn">默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='scss' scoped>
+.login{
+    background: #000;
+    .btn{
+        background: pink;
+    }
+}
+</style>
